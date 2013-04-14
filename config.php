c940e220dcdf994e6857a420bcb55f8225b2cecd
Piethein Strengholt <piethein@xbmcfreak.nl>
2013-04-14 14:35:16 +0200

    fix passwd

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 // Connection's Parameters
 $db_host="localhost";
 $db_name="openreader";
-$username="root";
-$password="voodoo2";
+$username="openreader";
+$password="openreader";
 
 mysql_connect($db_host, $username, $password) or die(mysql_error());
 mysql_select_db($db_name) or die(mysql_error());
