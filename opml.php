21ba7ba94f1c535d53f700fdb5f62d20cbee25bc
Piethein Strengholt <piethein@xbmcfreak.nl>
2013-05-30 16:24:16 +0200

    restore broken opml upload

--- opml.php
+++ opml.php
@@ -8,24 +8,20 @@ include 'functions.php';
 <html>
 <head>
  <link rel="stylesheet" href="stylesheets/styles.css">
- <script src="javascripts/jquery.min.js"></script>
+ <script src="javascripts/jquery-1.9.1.min.js"></script>
 </head>
+<body>
 
 <result>
 <div class="opml-form">
-
-<div class="alert alert-info">
-  <button type="button" class="close" data-dismiss="alert">&times;</button>
-  <strong>Coming from Google Reader?</strong>  Use Google Reader export to get your Google Reader subscriptions into a .xml file.
-</div>
+Coming from Google Reader? Use Google Reader export to get your Google Reader subscriptions into a .xml file.<br>
 
 <form action="opmlhandler.php" method="post" enctype="multipart/form-data">
- <fieldset>
-   <legend>OPML Wizard</legend>
-   <label>Filename</label>
-   <input type="file" title="Search for a file to add">
- </fieldset>
-<button type="submit" class="btn">Submit</button>
+<label for="file">Filename:</label>
+<input type="file" name="file" id="file"><br>
+<input type="submit" name="submit" value="Submit">
 </form>
 </div>
 </result>
+</body>
+</html>
