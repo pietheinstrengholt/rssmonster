a84069771ffb84a3ce44b4921f209a5bdfe454da
Piethein Strengholt <piethein@xbmcfreak.nl>
2014-01-10 13:15:06 +0100

    fix feedbar when empty result

--- feedbar.php
+++ feedbar.php
@@ -14,26 +14,28 @@ $status = get_json('{"jsonrpc": "2.0", "overview": "status"}');
 echo "<div class=\"panel\" id=\"status\">";
 echo "<a href=\"#\" class=\"list-group-item active\"><b>Status menu items</b></a>";
 
-foreach ($status as $row) {
+if (!empty($status)) {
+	foreach ($status as $row) {
 
-	if (!empty($row)) {
-		$cssid = str_replace(" ", "-", $row['name']);
-		echo "<a href=\"#\" id=\"$cssid\" class=\"list-group-item\">";
-		echo "<span class=\"badge\">$row[count]</span>";	
-		
-		if ($cssid == 'unread') {
-			echo "<span id=\"title-bar\"><span class=\"glyphicon glyphicon-search\"></span><span id=\"title-name\">$row[name]</span></span>";
-		} elseif ($cssid == 'read') {
-			echo "<span id=\"title-bar\"><span class=\"glyphicon glyphicon-pencil\"></span><span id=\"title-name\">$row[name]</span></span>";
-		} elseif ($cssid == 'starred') {
-			echo "<span id=\"title-bar\"><span class=\"glyphicon glyphicon-star\"></span><span id=\"title-name\">$row[name]</span></span>";
-		} elseif ($cssid == 'last-24-hours') {
-			echo "<span id=\"title-bar\"><span class=\"glyphicon glyphicon-tint\"></span><span id=\"title-name\">$row[name]</span></span>";
-		} elseif ($cssid == 'last-hour') {
-			echo "<span id=\"title-bar\"><span class=\"glyphicon glyphicon-leaf\"></span><span id=\"title-name\">$row[name]</span></span>";
+		if (!empty($row)) {
+			$cssid = str_replace(" ", "-", $row['name']);
+			echo "<a href=\"#\" id=\"$cssid\" class=\"list-group-item\">";
+			echo "<span class=\"badge\">$row[count]</span>";	
+			
+			if ($cssid == 'unread') {
+				echo "<span id=\"title-bar\"><span class=\"glyphicon glyphicon-search\"></span><span id=\"title-name\">$row[name]</span></span>";
+			} elseif ($cssid == 'read') {
+				echo "<span id=\"title-bar\"><span class=\"glyphicon glyphicon-pencil\"></span><span id=\"title-name\">$row[name]</span></span>";
+			} elseif ($cssid == 'starred') {
+				echo "<span id=\"title-bar\"><span class=\"glyphicon glyphicon-star\"></span><span id=\"title-name\">$row[name]</span></span>";
+			} elseif ($cssid == 'last-24-hours') {
+				echo "<span id=\"title-bar\"><span class=\"glyphicon glyphicon-tint\"></span><span id=\"title-name\">$row[name]</span></span>";
+			} elseif ($cssid == 'last-hour') {
+				echo "<span id=\"title-bar\"><span class=\"glyphicon glyphicon-leaf\"></span><span id=\"title-name\">$row[name]</span></span>";
+			}
+			
+			echo "</a>";
 		}
-		
-		echo "</a>";
 	}
 }
 
@@ -44,7 +46,7 @@ echo "<div class=\"panel\" id=\"categories\">";
 
 echo "<a href=\"#\" class=\"list-group-item\"><b>Categories items</b></a>";
 
-function header_section($input, $name) {
+function fn_header_section($input, $name) {
 
 	global $conn;
 
@@ -104,7 +106,10 @@ function header_section($input, $name) {
 
 // Get overview of all categories and call function to feed feedbar
 $array = get_json('{"jsonrpc": "2.0", "overview": "category-detailed"}');
-header_section($array,'category');
+
+if (!empty($array)) {
+	fn_header_section($array,'category');
+}
 
 echo "</div>";
 
