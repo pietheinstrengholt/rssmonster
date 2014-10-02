4c3a020dc156330bb197dbf226d3e05f4da05433
karibuTW <karibu@freedif.org>
2014-10-02 21:30:12 +0800

    Update config.php
    Replace PDO config to link to Connection's Parameters.
    Otherwise cause a blank page due to SQL miss configuration.

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $db_name="openreader";
 $username="admin";
 $password="apple200";
 
-$conn = new PDO('mysql:host=localhost;dbname=openreaderst;charset=utf8', $username, $password);
+$conn = new PDO('mysql:$db_host;$db_name;charset=utf8', $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
