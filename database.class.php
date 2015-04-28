970833bef389e959ce5cebdbe05b5458fae5466e
Piethein Strengholt <piethein@xbmcfreak.nl>
2015-04-28 17:35:03 +0200

    add SET NAMES utf8 to database class

--- database.class.php
+++ database.class.php
@@ -1,39 +1,40 @@
 <?php
 
 class Database {
-    private $host      = DB_HOST;
-    private $user      = DB_USER;
-    private $pass      = DB_PASS;
-    private $dbname    = DB_NAME;
+	private $host      = DB_HOST;
+	private $user      = DB_USER;
+	private $pass      = DB_PASS;
+	private $dbname    = DB_NAME;
 
-    private $dbh;
-    private $error;
+	private $dbh;
+	private $error;
 
-    private $stmt;
+	private $stmt;
  
     public function __construct(){
-        // Set DSN
-        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+		// Set DSN
+		$dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
         // Set options
-        $options = array(
-            PDO::ATTR_PERSISTENT    => true,
-            PDO::ATTR_ERRMODE       => PDO::ERRMODE_EXCEPTION
+		$options = array(
+			PDO::ATTR_PERSISTENT    => true,
+			PDO::ATTR_ERRMODE       => PDO::ERRMODE_EXCEPTION,
+			PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
         );
-        // Create a new PDO instanace
-        try{
+		// Create a new PDO instanace
+		try{
 			$this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
         }
-        // Catch any errors
-        catch(PDOException $e){
-            $this->error = $e->getMessage();
-        }
-    }
+		// Catch any errors
+		catch(PDOException $e){
+		$this->error = $e->getMessage();
+		}
+	}
 
-	public function query($query){
+	public function query($query) {
 		$this->stmt = $this->dbh->prepare($query);
 	}
 
-	public function bind($param, $value, $type = null){
+	public function bind($param, $value, $type = null) {
 		if (is_null($type)) {
 			switch (true) {
 				case is_int($value):
@@ -52,41 +53,41 @@ class Database {
 		$this->stmt->bindValue($param, $value, $type);
 	}
 
-	public function execute(){
+	public function execute() {
 		return $this->stmt->execute();
 	}
 
-	public function resultset(){
+	public function resultset() {
 		$this->execute();
 		return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 
-	public function single(){
+	public function single() {
 		$this->execute();
 		return $this->stmt->fetch(PDO::FETCH_ASSOC);
 	}
 
-	public function rowCount(){
+	public function rowCount() {
 		return $this->stmt->rowCount();
 	}
 
-	public function lastInsertId(){
+	public function lastInsertId() {
 		return $this->dbh->lastInsertId();
 	}
 
-	public function beginTransaction(){
+	public function beginTransaction() {
 		return $this->dbh->beginTransaction();
 	}
 
-	public function endTransaction(){
+	public function endTransaction() {
 		return $this->dbh->commit();
 	}
 
-	public function cancelTransaction(){
+	public function cancelTransaction() {
 		return $this->dbh->rollBack();
 	}
 
-	public function debugDumpParams(){
+	public function debugDumpParams() {
 		return $this->stmt->debugDumpParams();
 	}
 
