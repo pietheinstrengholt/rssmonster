ad1d3b2ee05cf4fbc620f58146d22ee12f7dbc4c
Piethein Strengholt <piethein@xbmcfreak.nl>
2015-04-29 12:23:48 +0200

    cleanup update

--- update.php
+++ update.php
@@ -15,13 +15,13 @@ $database = new Database();
 
 //limit to 2500 read articles history
 $database->beginTransaction();
-$database->query("DELETE FROM t_articles WHERE id NOT IN (SELECT * FROM (SELECT id FROM `t_articles` WHERE star_ind <> '1' AND STATUS = 'read' ORDER BY insert_date DESC LIMIT 0 , 2500) a UNION SELECT * FROM (SELECT id FROM `t_articles` WHERE star_ind =  '1') b UNION SELECT * FROM (SELECT id FROM `t_articles` WHERE status = 'unread') c)");
+$database->query("DELETE FROM t_articles WHERE id NOT IN (SELECT * FROM (SELECT id FROM t_articles WHERE star_ind <> '1' AND STATUS = 'read' ORDER BY insert_date DESC LIMIT 0 , 2500) a UNION SELECT * FROM (SELECT id FROM t_articles WHERE star_ind =  '1') b UNION SELECT * FROM (SELECT id FROM t_articles WHERE status = 'unread') c)");
 $database->execute();
 $database->endTransaction();
 
 //delete articles with no ref to feed_id
 $database->beginTransaction();
-$database->query("DELETE FROM `t_articles` WHERE feed_id NOT IN (SELECT DISTINCT id FROM t_feeds)");
+$database->query("DELETE FROM t_articles WHERE feed_id NOT IN (SELECT DISTINCT id FROM t_feeds)");
 $database->execute();
 $database->endTransaction();
 
@@ -31,20 +31,20 @@ $rows = $database->resultset();
 
 if (!empty($rows)) {
 
-	//previous week
+	//set previous week
 	$previousweek = date('Y-m-j H:i:s', strtotime('-7 days'));
 
-	echo "<table id=\"update\" class=\"table table-bordered table-condensed\">";
+	echo "<table border=\"1\"  id=\"update\" class=\"table table-bordered table-condensed\">";
 	echo "<tr>";
 	echo "<th>ID</th>";
 	echo "<th>Feed_Name</th>";
-	echo "<th>Last_date</th>";
 	echo "<th>Publish_date</th>";
 	echo "<th>Result</th>";
 	echo "</tr>";
 
 	foreach($rows as $row) {
 
+		//set feed variables
 		$feed_url  = $row['url'];
 		$feed_id   = $row['id'];
 		$feed_name = $row['feed_name'];
@@ -67,27 +67,7 @@ if (!empty($rows)) {
 			echo $feed->error();
 			echo "</div>";
 			
-		} else {
-
-			//select latest publish date from articles
-			$database->query("SELECT publish_date FROM t_articles WHERE feed_id = '$feed_id;' ORDER BY publish_date DESC LIMIT 1");
-			$publish_date = $database->single();
-			
-			//set comparedate if a result is returned
-			if (!empty($publish_date)) {
-				$comparedate = $publish_date['publish_date'];
-			}
-
-			//default to 1900 if no results exist
-			if (empty($comparedate)) {
-				$comparedate = "1900-01-01 00:00:00";
-			}
-		
-			//update feed's last_update date
-			$database->beginTransaction();
-			$database->query("UPDATE t_feeds SET last_update = CURRENT_TIMESTAMP WHERE id = '$feed_id'");
-			$database->execute();
-			$database->endTransaction();		
+		} else {		
 
 			foreach ($feed->get_items() as $item) {
 
@@ -102,42 +82,51 @@ if (!empty($rows)) {
 					$author = "";
 				}
 
-				echo "<tr><th>" . $feed_id . "</th><th>" . $feed_name . "</th><th>" . $comparedate . "</th><th>" . $date . "</th><th>";
+				echo "<tr><th>" . $feed_id . "</th><th>" . $feed_name . "</th><th>" . $date . "</th><th>";
 
 				$database->query("SELECT * FROM t_articles WHERE url = :url AND feed_id = :feed_id");
 				$database->bind(':url', $url);
 				$database->bind(':feed_id', $feed_id);
-				$resulturl = $database->single();
+				$resulturls = $database->resultset();
 
 				$database->query("SELECT * FROM t_articles WHERE subject = :subject AND feed_id = :feed_id");
 				$database->bind(':subject', $subject);
 				$database->bind(':feed_id', $feed_id);
-				$resultsubject = $database->single();
-				
-				//only add article if the publish date is newer than compare date, article is less than one week old and article subject or url is not present in database
-				if ((strtotime($date) > strtotime($comparedate)) && (!(strtotime($date) < strtotime($previousweek))) && empty($resulturl) && empty($resultsubject)) {
-					echo "New article found";
-					$database->beginTransaction();
-					$database->query("INSERT INTO t_articles (feed_id, status, url, subject, content, publish_date, author) VALUES ('$feed_id', 'unread', '$url', :subject, :content, '$date', '$author')");
-					$database->bind(':subject', $subject);
-					$database->bind(':content', $content);
-					$database->execute();
-					$database->endTransaction();					
-				}
-				
-				//debug message if article if more than one week old
-				if (strtotime($date) < strtotime($previousweek)) {
-					echo "Article more than one week old ";				
-				}
+				$resultsubjects = $database->resultset();
 				
 				//debug message if article is already present in database
-				if ((!empty($resulturl)) || (!empty($resultsubject))) {
-					echo "Article already present in database ";				
-				}				
+				if ((!empty($resulturls)) || (!empty($resultsubjects))) {
+					echo "Article already present in database";
+				//debug message if article is more than one week old					
+				} elseif (strtotime($date) < strtotime($previousweek)) {
+					echo "Article more than one week old";
+				//else add article
+				} else {
+					echo "New article found";
+					$database->beginTransaction();
+					$database->query("INSERT INTO t_articles (feed_id, status, url, subject, content, publish_date, author) VALUES (:feed_id, :status, :url, :subject, :content, :date, :author)");
+					$database->bind(':feed_id', $feed_id);
+					$database->bind(':status', 'unread');
+					$database->bind(':url', $url);
+					$database->bind(':subject', $subject);
+					$database->bind(':content', $content);
+					$database->bind(':date', $date);
+					$database->bind(':author', $author);
+					$database->execute();
+					$database->endTransaction();					
+				}		
 
 				echo "</th></tr>";
 
 			}
+			
+			//update feed's last_update date
+			$database->beginTransaction();
+			$database->query("UPDATE t_feeds SET last_update = CURRENT_TIMESTAMP WHERE id = :feed_id");
+			$database->bind(':feed_id', $feed_id);			
+			$database->execute();
+			$database->endTransaction();			
+			
 		}
 	}
 	echo "</table>";
