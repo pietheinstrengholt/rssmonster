a919b668ae7bf4838dad8f13b4678a3f1b0e7055
Piethein Strengholt <piethein@strengholt-online.nl>
2016-06-21 16:45:31 +0200

    remove stylesheet from blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,7 +49,7 @@
 				<div class="container">
 
 					<!-- Brand and toggle get grouped for better mobile display -->
-					<div class="navbar-header" style="margin-right:0px; margin-left:0px;">
+					<div class="navbar-header">
 						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 						<span class="sr-only">Toggle navigation</span>
 						<span class="icon-bar"></span>
