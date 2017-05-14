5539700e5437ecde1eb87d2be14843bb3348712f
Piethein Strengholt <piethein@strengholt-online.nl>
2017-05-14 12:28:07 +0200

    move unread, read and total count to Model

--- app/Category.php
+++ app/Category.php
@@ -3,16 +3,33 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Category extends Model
 {
-   protected $fillable = ['name'];
-   protected $table = 'categories';
+	protected $fillable = ['name'];
+	protected $table = 'categories';
+	protected $appends = array('unread_count', 'read_count', 'total_count');
 
-   public function feeds()
-   {
-      return $this->hasMany('App\Feed');
-   }
+	public function feeds()
+	{
+		return $this->hasMany('App\Feed')->orderBy('feed_name');
+	}
+
+	public function getUnreadCountAttribute()
+	{
+		return DB::table('feeds')->join('articles', 'feeds.id', '=', 'articles.feed_id')->where('feeds.category_id', $this->id)->where('articles.status', 'unread')->count();
+	}
+
+	public function getReadCountAttribute()
+	{
+		return DB::table('feeds')->join('articles', 'feeds.id', '=', 'articles.feed_id')->where('feeds.category_id', $this->id)->where('articles.status', 'unread')->count();
+	}
+
+	public function getTotalCountAttribute()
+	{
+		return DB::table('feeds')->join('articles', 'feeds.id', '=', 'articles.feed_id')->where('feeds.category_id', $this->id)->count();
+	}
 }
 
 ?>
--- app/Feed.php
+++ app/Feed.php
@@ -6,18 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Feed extends Model
 {
-   protected $fillable = ['feed_name','feed_desc','url','favicon'];
-   protected $table = 'feeds';
+	protected $fillable = ['feed_name','feed_desc','url','favicon'];
+	protected $table = 'feeds';
+	protected $appends = array('unread_count', 'read_count', 'total_count');
 
-   public function category()
-   {
-      return $this->belongsTo('App\Category');
-   }
+	public function category()
+	{
+		return $this->belongsTo('App\Category');
+	}
 
-   public function articles()
-   {
-      return $this->hasMany('App\Article');
-   }
+	public function articles()
+	{
+		return $this->hasMany('App\Article');
+	}
+
+	public function getUnreadCountAttribute()
+	{
+		return $this->articles->where('status','unread')->count();
+	}
+
+	public function getReadCountAttribute()
+	{
+		return $this->articles->where('status','read')->count();
+	}
+
+	public function getTotalCountAttribute()
+	{
+		return $this->articles->count();
+	}
 }
 
 ?>
