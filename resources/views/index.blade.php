a908cdbfc94bd0eceefbd6b5d34d5add8c4ccd90
Piethein Strengholt <piethein@strengholt-online.nl>
2018-01-25 19:46:45 +0100

    add bootstrap 4.0, fontawesome

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,25 +15,25 @@
 	<!-- CSS -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link rel="stylesheet" href="https://bootswatch.com/4/superhero/bootstrap.min.css" crossorigin="anonymous">
-	<script defer src="https://use.fontawesome.com/releases/v5.0.2/js/all.js"></script>
+	<link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+	<script defer src="{{ URL::asset('js/fontawesome-5.0.4.js') }}"></script>
 
 	<!-- Configuring Web Applications -->
-	<link rel="apple-touch-icon" sizes="57x57" href="http://localhost/rssmonster/public/img/favicon/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="http://localhost/rssmonster/public/favicon/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="http://localhost/rssmonster/public/favicon/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="http://localhost/rssmonster/public/favicon/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="http://localhost/rssmonster/public/favicon/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="http://localhost/rssmonster/public/favicon/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="http://localhost/rssmonster/public/favicon/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="http://localhost/rssmonster/public/favicon/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="http://localhost/rssmonster/public/favicon/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"	href="http://localhost/rssmonster/public/favicon/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="http://localhost/rssmonster/public/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="http://localhost/rssmonster/public/favicon/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="http://localhost/rssmonster/public/favicon/favicon-16x16.png">
+	<link rel="apple-touch-icon" sizes="57x57" href="{{ URL::asset('img/img/favicon/apple-icon-57x57.png') }}">
+	<link rel="apple-touch-icon" sizes="60x60" href="{{ URL::asset('img/favicon/apple-icon-60x60.png') }}">
+	<link rel="apple-touch-icon" sizes="72x72" href="{{ URL::asset('img/favicon/apple-icon-72x72.png') }}">
+	<link rel="apple-touch-icon" sizes="76x76" href="{{ URL::asset('img/favicon/apple-icon-76x76.png') }}">
+	<link rel="apple-touch-icon" sizes="114x114" href="{{ URL::asset('img/favicon/apple-icon-114x114.png') }}">
+	<link rel="apple-touch-icon" sizes="120x120" href="{{ URL::asset('img/favicon/apple-icon-120x120.png') }}">
+	<link rel="apple-touch-icon" sizes="144x144" href="{{ URL::asset('img/favicon/apple-icon-144x144.png') }}">
+	<link rel="apple-touch-icon" sizes="152x152" href="{{ URL::asset('img/favicon/apple-icon-152x152.png') }}">
+	<link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset('img/favicon/apple-icon-180x180.png') }}">
+	<link rel="icon" type="image/png" sizes="192x192"	href="{{ URL::asset('img/favicon/android-icon-192x192.png') }}">
+	<link rel="icon" type="image/png" sizes="32x32" href="{{ URL::asset('img/favicon/favicon-32x32.png') }}">
+	<link rel="icon" type="image/png" sizes="96x96" href="{{ URL::asset('img/favicon/favicon-96x96.png') }}">
+	<link rel="icon" type="image/png" sizes="16x16" href="{{ URL::asset('img/favicon/favicon-16x16.png') }}">
 	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="http://localhost/rssmonster/public/img/favicon/ms-icon-144x144.png">
+	<meta name="msapplication-TileImage" content="{{ URL::asset('img/favicon/ms-icon-144x144.png') }}">
 	<meta name="theme-color" content="#ffffff">
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 </head>
