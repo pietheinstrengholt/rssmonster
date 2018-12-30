226d5a98fd7158641bb6b1ed63199f6ce56b83ad
Piethein Strengholt <piethein@strengholt-online.nl>
2018-12-30 19:42:12 +0100

    use DB_HOSTNAME instead of DB_HOST

--- server/util/database.js
+++ server/util/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD, {
     dialect: 'mysql',
-    host: process.env.DB_HOST
+    host: process.env.DB_HOSTNAME
   });
 
 module.exports = sequelize;
