94fc0b4c394308162b6d03da61291352df954536
Piethein Strengholt <piethein@strengholt-online.nl>
2019-01-02 12:16:28 +0100

    add part 2 of fever api - #24

--- server/controllers/fever.js
+++ server/controllers/fever.js
@@ -46,7 +46,7 @@ exports.getFever = async (req, res, next) => {
     //return list with all unread article id's
     if ("unread_item_ids" in req.query) {
       var unread_item_ids = [];
-      articles = await Article.findAll({
+      const articles = await Article.findAll({
         attributes: ["id"],
         where: {
           status: 'unread'
@@ -67,7 +67,7 @@ exports.getFever = async (req, res, next) => {
     //return string/comma-separated list with id's from read and starred articles
     if ("saved_item_ids" in req.query) {
       var unread_item_ids = [];
-      articles = await Article.findAll({
+      const articles = await Article.findAll({
         attributes: ["id"],
         where: {
           star_ind: 1
@@ -85,6 +85,77 @@ exports.getFever = async (req, res, next) => {
       arr['saved_item_ids'] = unread_item_ids.join(", ");
     }
 
+    //return string/comma-separated list with id's from read and starred articles
+    if ("items" in req.query) {
+      //add total number of articles to arr
+      const total_articles = await Article.count();
+      arr['total_items'] = total_articles;
+
+      //create empty items array where all articles will be pushed to
+      var items = [];
+
+      //request specific items, a maximum of 50 specific items requested by comma-separated argument
+      if (req.query.with_ids) {
+        //list with id's is comma-separated, so transform to array
+        var arrayIds = req.query.with_ids.split(',');
+
+        articles = await Article.findAll({
+          where: {
+            id: arrayIds
+          }
+        });
+        //request 50 additional items using the highest id of locally cached items
+      } else if (req.query.since_id) {
+
+        articles = await Article.findAll({
+          where: {
+            id: {
+              [Op.gt]: req.query.since_id
+            }
+          },
+          limit: 50
+        });
+        //request 50 previous items using the lowest id of locally cached items
+      } else if (req.query.max_id) {
+
+        articles = await Article.findAll({
+          where: {
+            id: {
+              [Op.lt]: req.query.max_id
+            }
+          },
+          limit: 50
+        });
+        //if no argument is given provide total_items and up to 50 items
+      } else {
+        articles = await Article.findAll({
+          order: [
+            ['id', 'ASC']
+          ],
+          limit: 50
+        });
+      }
+
+      await articles.forEach(function (article) {
+        articleObject = {
+          id: String(article.id),
+          feed_id: parseInt(article.feedId),
+          title: article.subject,
+          author: '',
+          html: article.content,
+          url: article.url,
+          is_saved: parseInt(article.star_ind),
+          is_read: (article.status === 'read' ? 1 : 0),
+          created_on_time: Math.floor(article.published / 1000)
+        };
+        items.push(articleObject);
+      });
+
+      //add items to arr
+      arr['items'] = items;
+
+    }
+
     //when argument is links, don't return anything at this moment
     if ("links" in req.query) {
       //return empty array for this moment
@@ -94,7 +165,7 @@ exports.getFever = async (req, res, next) => {
     //favicons
     if ("favicons" in req.query) {
       var favicons = [];
-      feeds = await Feed.findAll({
+      const feeds = await Feed.findAll({
         order: [
           ['feed_name', 'ASC']
         ]
@@ -104,7 +175,7 @@ exports.getFever = async (req, res, next) => {
           feedObject = {
             id: String(feed.id),
             favicon: feed.favicon
-          }
+          };
           favicons.push(feedObject);
         });
       }
