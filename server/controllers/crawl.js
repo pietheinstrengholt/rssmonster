17d112c8353e6083ae1b4854030c0d6230cf502b
ThinkChaos <ThinkChaos@users.noreply.github.com>
2019-01-03 16:34:27 +0000

    server: Fix more error paths not returning a response (#32) (#33)

--- server/controllers/crawl.js
+++ server/controllers/crawl.js
@@ -20,6 +20,7 @@ exports.crawl = async (req, res, next) => {
     return res.status(200).json("Crawling background process started.");
   } catch (err) {
     console.log(err);
+    return res.status(500).json(err);
   }
 };
 
