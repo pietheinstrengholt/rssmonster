44b5fdd2821edf1bc6f1672dacee936853dd85e8
Piethein Strengholt <piethein@strengholt-online.nl>
2019-01-04 16:26:05 +0100

    use async await in category.js controller

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,100 +1,100 @@
 const Category = require("../models/category");
 const Feed = require("../models/feed");
 
-exports.getCategories = (req, res, next) => {
-  Category.findAll({
-      include: [{
-        model: Feed,
-        required: true
-      }],
-      order: ['category_order', 'name']
-    })
-    .then(categories => {
-      console.log(categories);
-      res.status(200).json({
-        categories: categories
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      return res.status(500).json(err);
+exports.getCategories = async (req, res, next) => {
+  try {
+    const categories = await Category.findAll({
+      include: [
+        {
+          model: Feed,
+          required: true
+        }
+      ],
+      order: ["category_order", "name"]
     });
+    return res.status(200).json({
+      categories: categories
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 };
 
-exports.getCategory = (req, res, next) => {
-  const categoryId = req.params.categoryId;
-  Category.findByPk(categoryId, {
-      include: [{
-        model: Feed,
-        required: true
-      }]
-    })
-    .then(category => {
-      console.log(category);
-      res.status(200).json({
-        category: category
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      return res.status(500).json(err);
+exports.getCategory = async (req, res, next) => {
+  try {
+    const categoryId = req.params.categoryId;
+    const category = await Category.findByPk(categoryId, {
+      include: [
+        {
+          model: Feed,
+          required: true
+        }
+      ]
     });
+    return res.status(200).json({
+      category: category
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 };
 
-exports.addCategory = (req, res, next) => {
-  const name = req.body.name;
-  const category_order = req.body.category_order;
-  Category.create({
+exports.addCategory = async (req, res, next) => {
+  try {
+    const name = req.body.name;
+    const category_order = req.body.category_order;
+    const category = await Category.create({
       name: name,
       category_order: category_order
-    })
-    .then(result => {
-      console.log(result);
-      return res.status(200).json(result);
-    })
-    .catch(err => {
-      console.log(err);
-      return res.status(500).json(err);
     });
+    return res.status(200).json(category);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 };
 
-exports.updateCategory = (req, res, next) => {
-  const categoryId = req.params.categoryId;
-  const name = req.body.name;
-  const category_order = req.body.category_order;
-  Category.findByPk(categoryId)
-    .then(category => {
-      if (!category) {
-        return res.status(404).json({
-          message: 'Category not found',
-        });
-      }
-      return category
-        .update({
-          name: req.body.name,
-          category_order: req.body.category_order,
-        })
-        .then(() => res.status(200).json(category))
-        .catch((error) => res.status(400).json(error));
-    })
-    .catch((error) => res.status(400).json(error));
+exports.updateCategory = async (req, res, next) => {
+  try {
+    const categoryId = req.params.categoryId;
+    const name = req.body.name;
+    const category_order = req.body.category_order;
+    category = await Category.findByPk(categoryId);
+    if (!category) {
+      return res.status(404).json({
+        message: "Category not found."
+      });
+    } else {
+      category.update({
+        name: req.body.name,
+        category_order: req.body.category_order
+      });
+      return res.status(200).json(category);
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 };
 
-exports.deleteCategory = (req, res, next) => {
-  const categoryId = req.params.categoryId;
-  Category.findByPk(categoryId)
-    .then(category => {
-      if (!category) {
-        return res.status(400).json({
-          message: 'Category not found',
-        });
-      }
-      return category
-        .destroy()
-        .then(() => res.status(204).json({
-          message: 'Deleted successfully',
-        }))
-        .catch((error) => res.status(400).json(error));
-    })
-    .catch((error) => res.status(400).json(error));
+exports.deleteCategory = async (req, res, next) => {
+  try {
+    const categoryId = req.params.categoryId;
+    category = await Category.findByPk(categoryId);
+    if (!category) {
+      return res.status(400).json({
+        message: "Category not found."
+      });
+    } else {
+      category.destroy();
+      return es.status(204).json({
+        message: "Deleted successfully."
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 };
