9c432af17fab97e11872a70d6b011396fe03592b
Piethein Strengholt <piethein@strengholt-online.nl>
2019-01-13 12:40:45 +0100

    replace request and feedparser by feedparser-promised

--- server/controllers/crawl.js
+++ server/controllers/crawl.js
@@ -4,17 +4,14 @@ const Op = Sequelize.Op;
 const Feed = require("../models/feed");
 const Article = require("../models/article");
 
-var FeedParser = require("feedparser");
-var request = require("request"); // for fetching the feed
-var zlib = require('zlib');
-var Iconv = require('iconv').Iconv;
+const feedparser = require("feedparser-promised");
 
 exports.crawl = async (req, res, next) => {
   try {
     const feeds = await Feed.findAll();
 
     if (feeds.length > 0) {
-      feeds.forEach(function (feed) {
+      feeds.forEach(function(feed) {
         fetch(feed);
         next();
       });
@@ -26,100 +23,19 @@ exports.crawl = async (req, res, next) => {
   }
 };
 
-function fetch(feed) {
-  // Define our streams
-  var req = request(feed.url, {
-    timeout: 10000,
-    pool: false
-  });
-  req.setMaxListeners(50);
-  // Some feeds do not respond without user-agent and accept headers.
-  req.setHeader(
-    "user-agent",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36"
-  );
-  req.setHeader("accept", "text/html,application/xhtml+xml");
-
-  var feedparser = new FeedParser();
-
-  // Define our handlers
-  req.on("error", (done) => feed.update({
-    errorCount: Sequelize.literal('errorCount + 1')
-  }));
-  req.on("response", function (res) {
-    if (res.statusCode != 200)
-      return this.emit("error", new Error("Bad status code"));
-    var encoding = res.headers["content-encoding"] || "identity",
-      charset = getParams(res.headers["content-type"] || "").charset;
-    res = maybeDecompress(res, encoding);
-    res = maybeTranslate(res, charset);
-    res.pipe(feedparser);
-  });
-
-  feedparser.on("error", (done) => feed.update({
-    errorCount: Sequelize.literal('errorCount + 1')
-  }));
-  feedparser.on("readable", function () {
-    var post;
-    while ((post = this.read())) {
-      //process article
-      processArticle(feed, post);
-    }
-  });
-}
-
-function done(err) {
-  if (err) {
-    console.log(err, err.stack);
-  }
-}
-
-function getParams(str) {
-  var params = str.split(";").reduce(function (params, param) {
-    var parts = param.split("=").map(function (part) {
-      return part.trim();
-    });
-    if (parts.length === 2) {
-      params[parts[0]] = parts[1];
-    }
-    return params;
-  }, {});
-  return params;
-}
-
-function maybeDecompress(res, encoding) {
-  var decompress;
-  if (encoding.match(/\bdeflate\b/)) {
-    decompress = zlib.createInflate();
-  } else if (encoding.match(/\bgzip\b/)) {
-    decompress = zlib.createGunzip();
-  }
-  return decompress ? res.pipe(decompress) : res;
-}
-
-function maybeTranslate(res, charset) {
-  var iconv;
-  // Use iconv if its not utf8 already.
-  if (!iconv && charset && !/utf-*8/i.test(charset)) {
-    try {
-      iconv = new Iconv(charset, "utf-8");
-      console.log("Converting from charset %s to utf-8", charset);
-      iconv.on("error", done);
-      // If we're using iconv, stream will be the output of iconv
-      // otherwise it will remain the output of request
-      res = res.pipe(iconv);
-    } catch (err) {
-      res.emit("error", err);
-    }
-  }
-  return res;
+async function fetch(feed) {
+  feedparser
+    .parse(feed.url)
+    .then(items => items.forEach(item => processArticle(feed, item)))
+    .catch(console.log(err));
 }
 
 async function processArticle(feed, post) {
   try {
     const article = await Article.findOne({
       where: {
-        [Op.or]: [{
+        [Op.or]: [
+          {
             url: post.link
           },
           {
