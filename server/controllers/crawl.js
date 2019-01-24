51ae590a48f5449925e2362148153d3a9c010f56
Piethein Strengholt <piethein@strengholt-online.nl>
2019-01-24 13:44:49 +0100

    push wip

--- server/controllers/crawl.js
+++ server/controllers/crawl.js
@@ -4,169 +4,42 @@ const Op = Sequelize.Op;
 const Feed = require("../models/feed");
 const Article = require("../models/article");
 
-var FeedParser = require("feedparser");
-var request = require("request"); // for fetching the feed
-var zlib = require("zlib");
-var Iconv = require("iconv").Iconv;
-
 const autodiscover = require("../util/autodiscover");
+const parseFeed = require("../util/parser");
+const faviconoclast = require("faviconoclast");
 
 exports.getCrawl = async (req, res, next) => {
   try {
     const feeds = await Feed.findAll();
 
     if (feeds.length > 0) {
-      feeds.forEach(function(feed) {
+      feeds.forEach(async function (feed) {
         fetch(feed);
       });
     }
-    return res.status(200).json("Crawling background process started.");
+    //return res.status(200).json("Crawling background process started.");
   } catch (err) {
     console.log(err);
-    return res.status(500).json(err);
+    //return res.status(500).json(err);
   }
 };
 
 async function fetch(feed) {
-  // Define our streams
-  const url = await autodiscover.discover(feed.rssUrl);
-  var req = request(url, {
-    timeout: 10000,
-    pool: false
-  });
-  req.setMaxListeners(50);
-  // Some feeds do not respond without user-agent and accept headers.
-  req.setHeader(
-    "user-agent",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36"
-  );
-  req.setHeader("accept", "text/html,application/xhtml+xml");
-
-  var feedparser = new FeedParser();
-
   try {
-    // Define our handlers
-    req.on("error", done =>
-      feed.update({
-        errorCount: Sequelize.literal("errorCount + 1")
-      })
-    );
+    const url = await autodiscover.discover(feed.rssUrl);
 
-    req.on("response", function(res) {
-      if (res.statusCode != 200) {
-        return this.emit("error", new Error("Bad status code"));
-      } else {
-        var encoding = res.headers["content-encoding"] || "identity",
-          charset = getParams(res.headers["content-type"] || "").charset;
-        res = maybeDecompress(res, encoding);
-        res = maybeTranslate(res, charset);
-        res.pipe(feedparser);
-      }
-    });
-
-    feedparser.on("error", done =>
-      feed.update({
-        errorCount: Sequelize.literal("errorCount + 1")
-      })
-    );
-    feedparser.on("readable", function() {
-      var post;
-      while ((post = this.read())) {
-        //process article
-        processArticle(feed, post);
-      }
-      //set errorCount back to 0
-      feed.update({
-        errorCount: 0
-      });
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
-}
-
-function done(err) {
-  if (err) {
-    console.log(err, err.stack);
-  }
-}
-
-function getParams(str) {
-  var params = str.split(";").reduce(function(params, param) {
-    var parts = param.split("=").map(function(part) {
-      return part.trim();
-    });
-    if (parts.length === 2) {
-      params[parts[0]] = parts[1];
-    }
-    return params;
-  }, {});
-  return params;
-}
-
-function maybeDecompress(res, encoding) {
-  var decompress;
-  if (encoding.match(/\bdeflate\b/)) {
-    decompress = zlib.createInflate();
-  } else if (encoding.match(/\bgzip\b/)) {
-    decompress = zlib.createGunzip();
-  }
-  return decompress ? res.pipe(decompress) : res;
-}
-
-function maybeTranslate(res, charset) {
-  var iconv;
-  // Use iconv if its not utf8 already.
-  if (!iconv && charset && !/utf-*8/i.test(charset)) {
-    try {
-      iconv = new Iconv(charset, "utf-8");
-      console.log("Converting from charset %s to utf-8", charset);
-      iconv.on("error", done);
-      // If we're using iconv, stream will be the output of iconv
-      // otherwise it will remain the output of request
-      res = res.pipe(iconv);
-    } catch (err) {
-      res.emit("error", err);
-    }
-  }
-  return res;
-}
-
-async function processArticle(feed, post) {
-  try {
-    const article = await Article.findOne({
-      where: {
-        [Op.or]: [
-          {
-            url: post.link
-          },
-          {
-            subject: post.title
-          }
-        ],
-        [Op.and]: {
-          feedId: feed.id
+    faviconUrl = await new Promise((resolve, reject) => {
+      faviconoclast(feed.rssUrl, (err, iconUrl) => {
+        if (err) {
+          reject(err)
         }
-      }
+        resolve(iconUrl)
+      })
     });
 
-    if (!article) {
-      //add article
-      Article.create({
-        feedId: feed.id,
-        status: "unread",
-        star_ind: 0,
-        url: post.link,
-        image_url: "",
-        subject: post.title,
-        content: post.description,
-        //contentSnippet: item.contentSnippet,
-        //author: item.author,
-        published: post.pubdate
-      });
-    }
-  } catch (err) {
+    const feeditem = await parseFeed.process(url);
+    console.log(feeditem);
+  } catch(err) {
     console.log(err);
   }
 }
---
+++ server/util/parser.js
@@ -0,0 +1,56 @@
+const FeedParser = require("feedparser");
+const got = require("got");
+const he = require("he");
+
+exports.process = async function(feedUrl) {
+  const stream = await ReadFeed(feedUrl)
+  const posts = await ReadFeedStream(stream, feedUrl)
+  const response = await ParseFeedPost(posts)
+  return response
+}
+
+async function ReadFeed (url) {
+  const response = await got.stream(url, { retries: 0 })
+  return response
+}
+
+async function ReadFeedStream (stream, feedUrl) {
+  const feed = {
+    meta: '',
+    posts: []
+  }
+  return new Promise((resolve, reject) => {
+    stream.pipe(new FeedParser())
+      .on('error', reject)
+      .on('end', () => {
+        resolve(feed)
+      })
+      .on('readable', function () {
+        const streamFeed = this
+        feed.meta = {
+          link: this.meta.link,
+          xmlurl: this.meta.xmlurl ? this.meta.xmlurl : feedUrl,
+          favicon: this.meta.favicon,
+          description: this.meta.description,
+          title: this.meta.title
+        }
+        let item
+        while ((item = streamFeed.read())) {
+          feed.posts.push(item)
+        }
+      })
+  })
+}
+
+function ParseFeedPost (feed) {
+  feed.posts.map((item) => {
+    item.favourite = false
+    item.read = false
+    item.offline = false
+    if (item.summary) {
+      item.summary = he.unescape(item.summary)
+    }
+    return item
+  })
+  return feed
+}
