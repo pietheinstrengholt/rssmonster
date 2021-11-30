ebf9241288d5f028085c2a5356774ce4a5c43737
Piethein Strengholt <piethein@strengholt-online.nl>
2021-11-30 12:29:37 +0100

    revert latest commit

--- server/util/autodiscover.js
+++ server/util/autodiscover.js
@@ -43,6 +43,10 @@ https.globalAgent.maxSockets = 1000000;
   return domain;
 } */
 
+function validateUrl(value) {
+  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+}
+
 //function to return overlap
 function findOverlap(a, b) {
   if (b.length === 0) {
@@ -95,9 +99,7 @@ async function getUrl(url) {
     };
 
     //fetch url by using proper user agent
-    if (isURL(url)) {
-      const response = await fetch(url, options);
-    }
+    const response = await fetch(url, options);
 
     if (response.ok) {
 
