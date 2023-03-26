be5eacd5cf97f3cb73da69dc1be99d374cc4b683
Piethein Strengholt <piethein@strengholt-online.nl>
2023-03-26 14:07:18 +0200

    install vue3 and compat service

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,12 +1,28 @@
 module.exports = {
-    pwa: {
-      themeColor: "#000000",
-      msTileColor: "#000000",
-      appleMobileWebAppCapable: 'yes',
-      appleMobileWebAppStatusBarStyle: 'black',
-      appleMobileWebAppCache: "yes",
-      manifestOptions: {
-        background_color: "#ffffff"
+pwa: {
+  themeColor: "#000000",
+  msTileColor: "#000000",
+  appleMobileWebAppCapable: 'yes',
+  appleMobileWebAppStatusBarStyle: 'black',
+  appleMobileWebAppCache: "yes",
+  manifestOptions: {
+    background_color: "#ffffff"
+  }
+},
+chainWebpack: config => {
+  config.resolve.alias.set('vue', '@vue/compat')
+  config.module
+    .rule('vue')
+    .use('vue-loader')
+    .tap(options => {
+      return {
+        ...options,
+        compilerOptions: {
+          compatConfig: {
+            MODE: 2
+          }
+        }
       }
-    }
-  };
+    })
+  }
+};
