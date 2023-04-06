7ee46e04011c14b6cd36897503f8fced00e08e40
Piethein Strengholt <piethein@strengholt-online.nl>
2023-04-06 16:50:03 +0200

    fix loading serviceworker

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -1,9 +1,8 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker'
-
-if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
+if (import.meta.env.VITE_NODE_ENV === 'production') {
+  register(`${import.meta.env.VITE_BASE_URL}service-worker.js`, {
     ready () {
       console.log(
         'App is being served from cache by a service worker.\n' +
