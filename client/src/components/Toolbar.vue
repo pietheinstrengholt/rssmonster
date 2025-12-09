6ef50deb767adc6d9a80a5019de30cc2c6b8897d
Piethein Strengholt <piethein@strengholt-online.nl>
2025-12-09 10:34:05 +0100

    Replace SVG icons with BootstrapIcon components
    Removed the local search.svg asset and updated Toolbar.vue to use BootstrapIcon for both the gear and search icons. Adjusted styles to position the new search icon and removed the background image reference for the search input.

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="toolbar">
     <div class="settings-icon" @click="settingsClicked" title="Settings">
-      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
-        <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
-      </svg>
+      <BootstrapIcon icon="gear-fill" size="20" />
     </div>
     <div class="status-toolbar" @click="toggleShowStatus">
       <p id="status">{{ capitalize($store.data.currentSelection.status) }}</p>
@@ -66,6 +64,9 @@
       method="post"
       role="url"
     >
+      <div class="search-icon-wrapper">
+        <BootstrapIcon icon="search" class="search-icon" />
+      </div>
       <input
         @keyup="emitSearchEvent()"
         v-model="search"
@@ -116,6 +117,25 @@
   float: left;
 }
 
+.search-wrap {
+  position: relative;
+}
+
+.search-icon-wrapper {
+  position: absolute;
+  left: 8px;
+  top: 50%;
+  transform: translateY(-50%);
+  display: flex;
+  align-items: center;
+  pointer-events: none;
+  color: #212325;
+}
+
+.search-icon {
+  font-size: 16px;
+}
+
 .search {
   width: 100%;
   height: 39px;
@@ -126,7 +146,6 @@
   border: none;
   line-height: 1;
   color: #212325;
-  background: url(../assets/images/search.svg) 8px 13px no-repeat;
 }
 
 .dropdownmenu {
