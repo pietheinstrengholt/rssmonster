3bdc5ac0fcb466d2c5287706ea66a14695c0d013
Piethein Strengholt <piethein@strengholt-online.nl>
2026-01-20 22:25:26 +0100

    Add test environment configuration
    Introduces a 'test' environment to the config file with MySQL settings and disables logging. This allows for separate database configuration during testing.

--- server/config/config.cjs
+++ server/config/config.cjs
@@ -11,6 +11,14 @@ module.exports = {
     dialect: 'mysql',
     logging: console.log
   },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
+    logging: false,
+  },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
