86a2d4b2e7207dcf127abb5135866c241d422d4c
Piethein Strengholt <piethein@strengholt-online.nl>
2026-02-10 19:17:06 +0100

    Don't reset article status in recluster
    Update reclusterForUser to only clear clusterId on Article.update and remove the previous status: 'unread' assignment. This preserves each article's existing status (e.g. read/unread) when reclustering so we don't inadvertently mark items as unread.

--- server/util/reclusterForUser.js
+++ server/util/reclusterForUser.js
@@ -64,7 +64,7 @@ export async function reclusterForUser(userId) {
    * -------------------------------------------------------------- */
 
   await Article.update(
-    { clusterId: null, status: 'unread' },
+    { clusterId: null },
     { where: { userId } }
   );
 
